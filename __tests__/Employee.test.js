2d5063f874823f75df98617f913670672f3d19ea
lssdavies <91108369+IslandBoyL@users.noreply.github.com>
2022-02-09 15:24:19 -0800

    wrote failing and passing test for Employee object

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,13 +1,30 @@
-//import Employee when running jest it will use the data in __mocks__
+//import Employee when running jest it will use the data in __mocks__/Employee.js
 const Employee = require("../lib/Employee");
 jest.mock("../lib/Employee");
 console.log(new Employee());
 
-///*Test function that takes 2 args i.e a descriptive string of the expected result and a call back arrow function*/
+/*Test function that takes 2 args i.e a descriptive string of the expected result and a call back arrow function. The below test is testing the properties of Employee*/
 test("creates an employee object", () => {
-  const employee = new Employee("Dave");
+  const employee = new Employee();
 
   expect(employee.name).toBe("Dave");
   expect(employee.id).toEqual(expect.any(Number));
+  expect(employee.email).toEqual(expect.any(String));
+});
+
+//These test will test Employee methods
+test("Gets name via getName()", () => {
+  const employee = new Employee();
+
+  expect(employee.name).toEqual(expect.any(String));
+});
+test("Gets id via getId()", () => {
+  const employee = new Employee();
+
+  expect(employee.id).toBeGreaterThan(0);
+});
+test("Gets email via getEmail()", () => {
+  const employee = new Employee();
+
   expect(employee.email).toEqual(expect.any(String));
 });
---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,31 @@
+//Importing required classes
+const Engineer = require("../lib/Engineer");
+const Employee = require("../lib/Employee");
+//importing mock data
+jest.mock("../lib/Employee");
+console.log(new Engineer());
+
+/*Test function that takes 2 args i.e a descriptive string of the expected result and a call back arrow function. The below test is testing the properties of Engineer*/
+test("creates an engineer object", () => {
+  const engineer = new Engineer("Dave", "Daveman");
+
+  expect(engineer.name).toBe("Dave");
+  expect(engineer.github).toEqual(expect.any(String));
+});
+
+// //These test will test Employee methods
+// test("Gets name via getName()", () => {
+//   const employee = new Employee();
+
+//   expect(employee.name).toEqual(expect.any(String));
+// });
+// test("Gets id via getId()", () => {
+//   const employee = new Employee();
+
+//   expect(employee.id).toBeGreaterThan(0);
+// });
+// test("Gets email via getEmail()", () => {
+//   const employee = new Employee();
+
+//   expect(employee.email).toEqual(expect.any(String));
+// });
